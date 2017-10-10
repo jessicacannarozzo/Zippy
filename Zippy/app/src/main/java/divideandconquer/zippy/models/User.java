3ac46067154aea009b9749f49314c4f4b750c72c
Geoffrey Duimovich <gaduimovich@gmail.com>
2017-10-10 17:21:01 -0400

    Added SignIn Activity that Moves Users to MainActivity

---
+++ Zippy/app/src/main/java/divideandconquer/zippy/models/User.java
@@ -0,0 +1,19 @@
+package divideandconquer.zippy.models;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+public class User {
+
+    public String username;
+    public String email;
+
+    public User() {
+    }
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+}
