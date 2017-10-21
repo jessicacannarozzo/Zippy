e6bedf38e38de755405ee7432b7c8c0cf4bbd7aa
Jess <jesscannarozzo@cmail.carleton.ca>
2017-10-21 15:47:22 -0400

    Moved TodoAdapter to its own class

--- Zippy/app/src/main/java/divideandconquer/zippy/models/TodoItem.java
+++ Zippy/app/src/main/java/divideandconquer/zippy/models/TodoItem.java
@@ -1,8 +1,11 @@
 package divideandconquer.zippy.models;
 
+import com.google.firebase.database.Exclude;
 import com.google.firebase.database.IgnoreExtraProperties;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @IgnoreExtraProperties
 public class TodoItem {
@@ -29,5 +32,16 @@ public class TodoItem {
 
     public void setChecked(Boolean checked) {
         this.checked = checked;
+    }
+
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("uid", uid);
+        result.put("author", author);
+        result.put("item", item);
+        result.put("checked", checked);
+
+        return result;
     }
 }
