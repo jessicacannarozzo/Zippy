31cc505425da2ffcd2ef8473f30c05b454fe5c77
Jess <jesscannarozzo@cmail.carleton.ca>
2017-11-20 18:35:01 -0500

    getting access list

---
+++ Zippy/app/src/main/java/divideandconquer/zippy/fragment/AccessListFragment.java
@@ -0,0 +1,30 @@
+package divideandconquer.zippy.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import divideandconquer.zippy.R;
+
+/**
+ * Created by navi on 20/11/17.
+ */
+
+
+public class AccessListFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.access_list_fragment, container, false);
+        return view;
+    }
+
+    private void getUsers() {
+        private DatabaseReference userList = FirebaseDatabase.getInstance().getReference()
+    }
+}
